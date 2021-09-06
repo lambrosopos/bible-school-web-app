b764429fd9422bc906d34db9fb65c0340dad3a29
qetroux <lambrosopos@gmail.com>
2021-09-06 23:34:26 +0900

    fix main_func

--- bible_school/app/views.py
+++ bible_school/app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def main_page(request):
-    return render('index.html')
+    return render(request, 'index.html')
