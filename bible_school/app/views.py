61e5b482f6f007783b0e510c16a21df359b6d654
qetroux <lambrosopos@gmail.com>
2021-09-09 23:25:02 +0900

    connect views with templates

--- bible_school/app/views.py
+++ bible_school/app/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
+
+def registration_page(request):
+    return render(request, 'pages/registration.html')
+
+def lookup_page(request):
+    return render(request, 'pages/lookup.html')
