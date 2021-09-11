929a4a2abaeb1f48cc5e32b54a133e0b66969f1a
qetroux <lambrosopos@gmail.com>
2021-09-11 22:37:28 +0900

    add student model

--- bible_school/app/models.py
+++ bible_school/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64, blank=True)
+    title = models.CharField(max_length=128, blank=True)
+    contact = models.IntegerField()
+    memo = models.TextField(blank=True)
