21a78c68600078b5de715d8f63d79c36de78a05d
qetroux <lambrosopos@gmail.com>
2021-09-11 22:41:14 +0900

    add student model to view

--- bible_school/app/views.py
+++ bible_school/app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Student
 
 def main_page(request):
     return render(request, 'pages/index.html')
 
-def registration_page(request):
-    return render(request, 'pages/registration.html')
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ('name', 'title', 'contact', 'memo')
 
 def lookup_page(request):
     return render(request, 'pages/lookup.html')
