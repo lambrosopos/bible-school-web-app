cb8b5f148bd69e95775d231a28a593e11aeba1c8
qetroux <lambrosopos@gmail.com>
2021-09-13 22:24:26 +0900

    finalizing forms and models

--- bible_school/app/forms.py
+++ bible_school/app/forms.py
@@ -6,5 +6,25 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = [ 'name', 'title', 'contact', 'memo' ]
-
-
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "placeholder":"이름을 입력해주세요 (예: 홍길동)"
+                }
+            ),
+            "title": forms.TextInput(
+                attrs={
+                    "placeholder":"현재 담당하고 있는 직책을 입력해주세요 (예: 권사)"
+                }
+            ),
+            "contact": forms.TextInput(
+                attrs={
+                    "placeholder":"개인 연락처를 적어주세요 (예: 010-1234-4321)"
+                }
+            ),
+            "memo": forms.Textarea(
+                attrs={
+                    "placeholder":"추가로 필요한 메모가 있다면 적어주세요"
+                }
+            )
+        }
--- bible_school/app/models.py
+++ bible_school/app/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
 class Student(models.Model):
+
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=64, blank=True)
+    name = models.CharField(max_length=64)
     title = models.CharField(max_length=128, blank=True)
-    contact = models.IntegerField()
+    
+    contact = PhoneNumberField(unique=True, null=False, blank=False, region='KR')
+
     memo = models.TextField(blank=True)
