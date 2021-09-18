d246d808144ccaad5e1dde96d2f1a429436432b6
lambrosopos <lambrosopos@gmail.com>
2021-09-19 02:43:31 +0900

    add rolegroup to admin dashboard

--- bible_school/app/admin.py
+++ bible_school/app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Student, Church, Title, Cohort
+from .models import ( 
+    Student, 
+    Church,
+    Title,
+    Cohort,
+    RoleGroupJoin
+)
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("name", "title", "contact", "memo")
@@ -14,7 +20,11 @@ class TitleAdmin(admin.ModelAdmin):
 class CohortAdmin(admin.ModelAdmin):
     list_display = ("name", "memo")
 
+class RoleGroupJoinAdmin(admin.ModelAdmin):
+    list_diplay = ("name")
+
 admin.site.register(Title, TitleAdmin)
+admin.site.register(RoleGroupJoin, RoleGroupJoinAdmin)
 admin.site.register(Church, ChurchAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Cohort, CohortAdmin)
