7f88fe7f50f18cc0d5672a05dc9e84896c780884
lambrosopos <lambrosopos@gmail.com>
2021-09-19 00:35:44 +0900

    fixing church - student relationship

--- bible_school/app/admin.py
+++ bible_school/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student, Church
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", "title", "contact", "memo")
+
+class ChurchAdmin(admin.ModelAdmin):
+    list_display = ("name", "contact")
+
+admin.site.register(Church, ChurchAdmin)
+admin.site.register(Student, StudentAdmin)
--- bible_school/app/models.py
+++ bible_school/app/models.py
@@ -3,20 +3,28 @@ from django.core.validators import RegexValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
-
-class Student(models.Model):
-
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=64)
-    title = models.CharField(max_length=128, blank=True)
-    
-    contact = PhoneNumberField(unique=True, null=False, blank=False, region='KR')
-
-    memo = models.TextField(blank=True)
-
-
 class Church(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128)
     contact = PhoneNumberField(unique=True, region='KR')
 
+    class Meta:
+        verbose_name_plural = "churches"
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=128, blank=True)
+    contact = PhoneNumberField(unique=True, null=False, blank=False, region='KR')
+    memo = models.TextField(blank=True)
+    church = models.ForeignKey(
+        Church,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name_plural = "students"
+
+    def __str__(self):
+        return self.name
